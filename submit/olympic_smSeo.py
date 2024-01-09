b49aa10c814f90e2f9d48104f6f4d9a1a1ee2e91
ByeolMaru <89494612+Sumin-Byeol@users.noreply.github.com>
2024-01-09 11:04:10 +0900

    final fix olympic.py

--- submit/olympic_smSeo.py
+++ submit/olympic_smSeo.py
@@ -10,34 +10,33 @@ def calculate_medal(medal_dict: dict, country: str, medal: str) -> dict:
     """
     if country not in medal_dict:
         medal_dict[country] = {"금메달": 0, "은메달": 0, "동메달": 0}
-    
     medal_dict[country][medal] += 1
     return medal_dict
-assert calculate_medal({}, "한국", "금매달") == {"한국": {"금매달: 1"}}
+#assert calculate_medal({}, "한국", "금매달") == {"한국": {"금매달: 1"}}
 
-# def display_progress(medal_data):
-#     """
-#     올림픽 medal을 country별로 집계하는 동안 진행 상황을 발표합니다.
-#     medal_data: ('country', 'medal')이 포함된 튜플 목록
-#     return: country를 키로 하고 medal 집계 결과를 값으로 하는 사전
-#     """
-#     medal_dict = {}
-#     total = len(medal_data)
+def display_progress(medal_data):
+    """
+    올림픽 medal을 country별로 집계하는 동안 진행 상황을 발표합니다.
+    medal_data: ('country', 'medal')이 포함된 튜플 목록
+    return: country를 키로 하고 medal 집계 결과를 값으로 하는 사전
+    """
+    medal_dict = {}
+    total = len(medal_data)
 
-#     for index, (country, medal) in enumerate(medal_data, start=1):
-#         medal_dict = calculate_medal(medal_dict, country, medal)
-#         print(f"{index} / {total} 메달 처리 중...")
+    for index, (country, medal) in enumerate(medal_data, start=1):
+        medal_dict = calculate_medal(medal_dict, country, medal)
+        print(f"{index} / {total} 메달 처리 중...")
 
-#     return medal_dict
+    return medal_dict
 
 
-# def print_medals(medal_dict):
-#     result = []
-#     for country, medals in medal_dict.items():
-#         result.append(
-#             f"{country}은(는) 금메달 {medals['금메달']}개, 은메달 {medals['은메달']}개, 동메달 {medals['동메달']}개 획득했습니다."
-#         )
-#     return "\n".join(result)
+def print_medals(medal_dict):
+    result = []
+    for country, medals in medal_dict.items():
+        result.append(
+            f"{country}은(는) 금메달 {medals['금메달']}개, 은메달 {medals['은메달']}개, 동메달 {medals['동메달']}개 획득했습니다."
+        )
+    return "\n".join(result)
 
 
 medal_data = [
@@ -48,7 +47,7 @@ medal_data = [
 ]
 
 
-# medal_dict = display_progress(medal_data)
-# sentences = print_medals(medal_dict)
-# print(sentences)
-# print("메달 획득 국가: ", ", ".join(re.findall(r"[가-힣]{2,}(?=은)", sentences)))
+medal_dict = display_progress(medal_data)
+sentences = print_medals(medal_dict)
+print(sentences)
+print("메달 획득 국가: ", ", ".join(re.findall(r"[가-힣]{2,}(?=은)", sentences)))
